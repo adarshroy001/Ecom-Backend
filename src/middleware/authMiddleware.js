833efc24d9d4d656e0c78b07d4370db09aca051c
Adarsh Roy <adarshroy2010@gmail.com>
2025-01-23 20:54:52 +0530

    Authmiddleware

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,15 @@
+import jwt from 'jsonwebtoken';
+
+export default isAuthenticated = (req,res,next)=>{
+    const token = req.header('Authorization'); // Expecting 'Bearer <token>'
+    if (!token) return res.status(401).json({ message: 'Access denied. No token Found . Check isAuthenticated' });
+
+    try {
+        const decoded = jwt.verify(token.split(" ")[1], process.env.JWT_SECRET);
+        req.user = decoded; // Attach user info to the request object
+        next();
+    } catch (error) {
+        res.status(400).json({ message: 'Invalid token.' });
+    }
+
+}
