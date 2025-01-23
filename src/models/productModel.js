950c0cf5b6b101ac9d6df7b26fcd1342a04aff87
Adarsh Roy <adarshroy2010@gmail.com>
2025-01-23 21:10:38 +0530

    PoductModel

---
+++ src/models/productModel.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    description: {
+        type: String,
+        default: '',
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    stock: {
+        type: Number,
+        default: 0, // Tracks available quantity
+    },
+    category: {
+        type: String,
+        enum: [
+            'Food:Veg', 
+            'Food:Non-Veg', 
+            'College Essentials', 
+            'Cakes:With Egg', 
+            'Cakes:No Egg', 
+            'Fashion:Men', 
+            'Fashion:Women', 
+            'Fashion:Accessories', 
+            'Bags', 
+            'Footwear:Men', 
+            'Footwear:Women'
+        ],
+        required: true,
+    },
+    image: {
+        type: String,
+        default: '', // URL or path to the product image
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+const Product = mongoose.model('Product', productSchema);
+export default Product;
