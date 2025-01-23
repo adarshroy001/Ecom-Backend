f72a6a04c51c19b3ca82db0ab8feb583313caa9f
Adarsh Roy <adarshroy2010@gmail.com>
2025-01-23 14:26:15 +0530

    Logout controller done

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -37,33 +37,37 @@ export const registerUser = async (req, res) => {
     res.status(500).json({ message: 'Facing Error in Signup Controller . Something went wrong.', error: error.message });
   }
 };
-
 // Login User
 export const loginUser = async (req, res) => {
- const {email , password} = req.body ; 
- try{
-   //checking is user exits with this email 
-   const existingUser = await User.findOne({email}) ;
-   if(!existingUser){
-    return res.status(404).json({message: 'User is not registered with This email'})
-   }
-     //checking password 
-     const isPasswordValid = await bcrypt.compare(password, existingUser.password);
-     if(!isPasswordValid) return res.status(400).json({message: 'Incorrect Password please Try again'})
+  const { email, password } = req.body;
+  try {
+    //checking is user exits with this email 
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      return res.status(404).json({ message: 'User is not registered with This email' })
+    }
+    //checking password 
+    const isPasswordValid = await bcrypt.compare(password, existingUser.password);
+    if (!isPasswordValid) return res.status(400).json({ message: 'Incorrect Password please Try again' })
     const token = jwt.sign({ id: existingUser._id, role: existingUser.role }, process.env.JWT_SECRET, { expiresIn: '7d' });
-    res.cookie('token',token, {
+    res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      maxAge: 7 * 24 * 60 * 60 * 1000,});
-      res.status(200).json({ message: 'Login successful.', user: { id: user._id, name: user.name, email: user.email, role: user.role } });
- } catch (error) {
-  res.status(500).json({ message: 'Facing Error in Login Controller . Something went wrong.', error: error.message });
-}
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+    res.status(200).json({ message: 'Login successful.', user: { id: user._id, name: user.name, email: user.email, role: user.role } });
+  } catch (error) {
+    res.status(500).json({ message: 'Facing Error in Login Controller . Something went wrong.', error: error.message });
+  }
 };
-
 // Logout User
 export const logoutUser = (req, res) => {
-
+  try {
+    res.clearCookie('token');
+    res.status(200).json({ message: 'Logout successful.' });
+  } catch (error) {
+    res.status(500).json({ message: 'Facing Error in Login Controller . Something went wrong.', error: error.message });
+  }
 };
 
 
